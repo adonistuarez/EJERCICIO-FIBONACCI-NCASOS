3be9f269ac67b6ba60495364bf7b6a68d82ccaff
adonistuarez <adonistuarezv@gmail.com>
2024-06-04 10:17:34 -0500

    Update fibonacci

--- miprimerproyecto 2/mifecha/src/Main1.java
+++ miprimerproyecto 2/mifecha/src/Main1.java
@@ -1,5 +1,22 @@
-public class Main1 {
-    public static void main1 (String [] args){
+import java.util.Scanner;
+
+public class SeriedeFibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number");
+        int n = scanner.nextInt();
+        int num1 = 0;
+        int acum = 0;
+        int num2 = 1;
+        System.out.print( num1 + "  " );
+        System.out.print( num2 + "  " );
+        for (int i = 0; i <= n; i++ ) {
+            acum = (num1 + num2);
+            System.out.print( acum + "  " );
+            num1 = num2;
+            num2 = acum;
+
+        }
 
     }
 }
