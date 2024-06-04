d48107703de45d5e94c2fc75dc8de704797ca521
user <user>
2024-06-04 09:41:55 -0500

    first commit

---
+++ miprimerproyecto 2/mifecha/src/MiFecha.java
@@ -0,0 +1,30 @@
+public class MiFecha {
+    private String dia;
+    private String mes;
+    private Int anio;
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public Int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(Int anio) {
+        this.anio = anio;
+    }
+
+}
